5009ee508d7630bd803d9b79faa14046827c5c91
shashankp <shashank.pv@gmail.com>
2016-12-06 20:09:24 +0530

    Add project files.

---
+++ WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var url = new Uri(textBox.Text);
+                //Use Windows credentials
+                using (var httpClient = new HttpClient(new HttpClientHandler
+                {
+                    UseDefaultCredentials = true
+                }))
+                {
+                    var result = httpClient.GetStringAsync(url).Result;
+                    var j = JObject.Parse(result);
+                    Console.WriteLine(j);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
