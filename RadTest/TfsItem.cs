b49b9307056152ab8d54067114f01b5fb7cf2974
shashankp <shashank.pv@gmail.com>
2016-12-07 09:58:05 +0530

    added telerik treemap sample

---
+++ RadTest/TfsItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadTest
+{
+    public class TfsItem
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
