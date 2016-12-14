612392139333de4bb43becb7a297c84d4e1cb419
shashankp <shashank.pv@gmail.com>
2016-12-14 23:28:40 +0530

    adding coloring scheme

--- WpfApplication2/Views/TreeMapSelection.xaml.cs
+++ WpfApplication2/Views/TreeMapSelection.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +29,34 @@ namespace WpfApplication2
             InitializeComponent();
             this.DataContext = TfsHelper.GetData();
             SubItems = TfsHelper.GetData();
+        }
+    }
+
+    public class ScoreToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = value as TfsItemViewModel;
+            if (result != null)
+            {
+                var score = result.BugCount;
+                SolidColorBrush brush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF0C6BBF"));
+                if (score >= 7)
+                {
+                    brush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFE10000"));
+                } else if (score >= 2 && score < 7)
+                {
+                    brush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFFFE100"));
+                }
+
+                return brush;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
